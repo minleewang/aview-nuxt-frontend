404edc27de0cb20341936f160a686f98e695f3fc
kimhyeonu4586 <shhw1103@naver.com>
2025-02-25 11:28:22 +0900

    feat:[김현우] homethird 변경

---
+++ AV-UI/pages/components/HomeFifth.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="footer">
+    <div class="footer-container">
+      <div class="footer-logo-area">
+        <img class="footer-AIV.LOGO.png" :src="'/fixed/AIV.LOGO.png'" alt="AIV.LOGO.png" />
+      </div>
+      <div class="footer-content">
+        <p>서울 금천구 가산디지털1로 25 18층 플레이데이터 (가산동 670)</p>
+        <p>최병준 | 양의정 | 왕환민 | 김현우 | 최민수</p>
+        <p>SK-networks Family AI Camp 8st - Final Project AIV Team</p>
+      </div>
+    </div>
+  </div>
+</template>
