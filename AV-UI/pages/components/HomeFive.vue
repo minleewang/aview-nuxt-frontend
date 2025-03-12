ee1ba6696de4463659c45ebdcab529f495461053
왕환민 <mini_wang@naver.com>
2025-03-12 14:31:38 +0900

    fix:[왕환민] 홈 화면 스크롤 수정[AVN-4]

--- AV-UI/pages/components/HomeFive.vue
+++ AV-UI/pages/components/HomeFive.vue
@@ -8,27 +8,47 @@
       <div class="grid-container">
         <div class="grid-item">
           <div class="title">면접에서 자신감 UP🔥</div>
-          <div class="description">JOBSTICK과 함께하는 모의 면접을 통해 다양한 면접 질문을 사전에 대응하며 불필요한 실수들을 사전에 처리할 수 있습니다. 이러한 과정을 반복할 수 있으며 완벽한 면접 준비 상태를 도와드립니다.</div>
+          <div class="description">
+            JOBSTICK과 함께하는 모의 면접을 통해 다양한 면접 질문을 사전에
+            대응하며 불필요한 실수들을 사전에 처리할 수 있습니다. 이러한 과정을
+            반복할 수 있으며 완벽한 면접 준비 상태를 도와드립니다.
+          </div>
         </div>
         <div class="grid-item">
           <div class="title">예측 질문 파악 OK🔍</div>
-          <div class="description">AI를 기반으로 업무별, 직무별 맞춤형 질문을 제공하기 때문에 실제 면접에서 이뤄질 수 있는 질문들을 사전에 제공하여 최적의 답변을 만들 수 있게 제공해드립니다.</div>
+          <div class="description">
+            AI를 기반으로 업무별, 직무별 맞춤형 질문을 제공하기 때문에 실제
+            면접에서 이뤄질 수 있는 질문들을 사전에 제공하여 최적의 답변을 만들
+            수 있게 제공해드립니다.
+          </div>
         </div>
         <div class="grid-item">
           <div class="title">다양한 기업 분석 CHECK✅️</div>
-          <div class="description">JOBSTICK에서는 다양한 분야와 직무, 각 기업의 면접 패턴과 요구사항을 분석해드며 해당 기업에 맞는 면접 준비를 도와드립니다.</div>
+          <div class="description">
+            JOBSTICK에서는 다양한 분야와 직무, 각 기업의 면접 패턴과 요구사항을
+            분석해드며 해당 기업에 맞는 면접 준비를 도와드립니다.
+          </div>
         </div>
         <div class="grid-item">
           <div class="title">효율적인 면접 준비 ON🔋</div>
-          <div class="description">다양한 질문과 정확한 피드백, 기업 분석등 면접 준비에 필요한 요소들을 제공하여 쉽고 빠른 면접 준비를 이용할 수 있습니다.</div>
+          <div class="description">
+            다양한 질문과 정확한 피드백, 기업 분석등 면접 준비에 필요한 요소들을
+            제공하여 쉽고 빠른 면접 준비를 이용할 수 있습니다.
+          </div>
         </div>
         <div class="grid-item">
           <div class="title">불필요한 면접 실수 CUT✂️</div>
-          <div class="description">본인의 면접 자세나 발화 습관을 녹화본으로 볼 수 있어 실제 면접에서 불필요한 실수들을 사전에 처리할 수 있도록 도와드립니다.</div>
+          <div class="description">
+            본인의 면접 자세나 발화 습관을 녹화본으로 볼 수 있어 실제 면접에서
+            불필요한 실수들을 사전에 처리할 수 있도록 도와드립니다.
+          </div>
         </div>
         <div class="grid-item">
           <div class="title">최적의 면접 대응 답변 READY📝</div>
-          <div class="description">AI를 이용한 정확한 피드백을 통해 부족한 점을 극복할 수 있으며 점차 면접 질문에 완벽한 대응을 할 수 있도록 도와드립니다.</div>
+          <div class="description">
+            AI를 이용한 정확한 피드백을 통해 부족한 점을 극복할 수 있으며 점차
+            면접 질문에 완벽한 대응을 할 수 있도록 도와드립니다.
+          </div>
         </div>
       </div>
     </div>
@@ -37,7 +57,7 @@
 
 <script>
 export default {
-  name: "Page5",
+  name: "HomeFive",
 };
 </script>
 
@@ -87,7 +107,12 @@ h1 {
 
 /* 그리드 아이템 스타일 */
 .grid-item {
-  background-color: rgba(240, 240, 240, 0.9); /* 배경을 반투명하게 설정하여 배경 이미지가 살짝 보이도록 */
+  background-color: rgba(
+    240,
+    240,
+    240,
+    0.9
+  ); /* 배경을 반투명하게 설정하여 배경 이미지가 살짝 보이도록 */
   padding: 20px; /* 패딩을 조금 더 늘려서 아이템 크기를 키움 */
   text-align: center;
   border-radius: 8px;
@@ -117,5 +142,4 @@ h1 {
   flex-grow: 1; /* 설명이 가능한 공간을 차지하도록 설정 */
   margin-top: 10px; /* 마진을 약간 줄여서 설명 간격 줄임 */
 }
-
 </style>
