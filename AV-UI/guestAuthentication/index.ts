79fe490c5adc6593a8e4eeb93cb8bbe66fb38fde
왕환민 <mini_wang@naver.com>
2025-04-25 17:21:31 +0900

    fix:[왕환민] 게스트 로그인 후 홈화면 이동[AVN-48]

--- AV-UI/guestAuthentication/index.ts
+++ AV-UI/guestAuthentication/index.ts
@@ -10,17 +10,6 @@ export default defineNuxtModule({
   setup(moduleOptions, nuxt) {
     const themeDir = resolve(__dirname, "..");
 
-    nuxt.hook("pages:extend", (pages) => {
-      pages.push({
-        name: "guestRedirection",
-        path: "/guest-oauth/redirect-access-token",
-        file: resolve(
-          themeDir,
-          "guestAuthentication/redirection/GuestRedirection.vue"
-        ),
-      });
-    });
-
     nuxt.hook("imports:dirs", (dirs) => {
       dirs.push(resolve(__dirname, "store"));
     });
